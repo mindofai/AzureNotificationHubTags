a14453538892351ccb5c58a48a35789758c92ede
Bryan Anthony Garcia <bryan@xam.com.au>
2022-06-16 16:00:24 +1000

    Initial changes

---
+++ AzureNotificationHubTags/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Azure.NotificationHubs;
+using Nito.AsyncEx;
+using System;
+using System.Linq;
+
+namespace AzureNotificationHubTags
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AsyncContext.Run(() => MainAsync(args));
+        }
+
+        static async void MainAsync(string[] args)
+        {
+            NotificationHubClient hub = NotificationHubClient
+                              .CreateClientFromConnectionString("<Notification Hub Namespace Connection String>", "<Notification hub name>");
+            var list = await hub.GetAllRegistrationsAsync(1000);
+            
+            if (list.Count() > 0)
+            {
+                foreach (var item in list)
+                {
+                    Console.WriteLine(string.Join(",", item.Tags));
+                }
+            }
+        }
+
+    }
+}
